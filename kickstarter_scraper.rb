a5c5000ad0706360d35435aeb9b198095ba10f28
Iskra B <ibatistapoblete@gmail.com>
2019-05-29 06:08:07 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,6 @@
 # require libraries/modules here
 require 'Nokogiri'
+require 'pry'
 def create_project_hash
   # write your code here
     html = File.read('fixtures/Kickstarter.html')
@@ -8,3 +9,4 @@ def create_project_hash
 
   end
 end
+binding.pry
