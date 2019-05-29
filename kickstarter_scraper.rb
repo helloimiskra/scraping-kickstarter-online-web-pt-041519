3c9b80f75fe1606aa3d373607431dece35bc11f2
Iskra B <ibatistapoblete@gmail.com>
2019-05-29 06:09:25 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,13 +3,12 @@ require 'Nokogiri'
 require 'pry'
 def create_project_hash
   # write your code here
-    html = File.read('fixtures/Kickstarter.html')
+  html = File.read('fixtures/Kickstarter.html')
 
-    kickstarter = Nokogiri::HTML(html)
+  kickstarter = Nokogiri::HTML(html)
 
-  end
-  binding.pry
+
 end
-
+  binding.pry
 
 create_project_hash
